efb09c1d40ea0fc70e04e6242ea2f80bc28c531e
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-10-21 10:03:44 +0700

    drop

--- chatbox/src/components/ChatRoom/UserInfo.js
+++ chatbox/src/components/ChatRoom/UserInfo.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import {auth} from '../../firebase/config';
 import {db} from '../../firebase/config';
 import { collection, doc, setDoc, addDoc, getDocs } from "firebase/firestore";
+import {AuthContext} from '../../Context/AuthProvider';
 
 const WrapperStyled = styled.div`
     display: flex;
@@ -19,27 +20,29 @@ const WrapperStyled = styled.div`
 
 export default function UserInfo() {
 
-    useEffect(()=>{
-        const result = collection(db,'users')
-        getDocs(result)
-                .then((snapshot) => {
-                    let user = [];
-                    
-                    snapshot.docs.forEach(doc => {
-                        user.push({...doc.data(),id: doc.id})
-                    })
-                    console.log(user);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-    },[])
-
+    // useEffect(()=>{
+    //     const result = collection(db,'users')
+    //     getDocs(result)
+    //             .then((snapshot) => {
+    //                 let user = [];
+    //                 snapshot.docs.forEach(doc => {
+    //                     user.push({...doc.data(),id: doc.id})
+    //                 })
+    //                 console.log(user);
+    //             })
+    //             .catch(error => {
+    //                 console.log(error);
+    //             })
+    // },[])
+    const { user: {
+        displayName,
+        photoURL
+    }} = React.useContext(AuthContext)
     return (
         <WrapperStyled>
             <div>
-                <Avatar>A</Avatar>
-                <Typography.Text className="username">ABC</Typography.Text>
+                <Avatar src={photoURL}>ABC</Avatar>
+                <Typography.Text className="username">{displayName}</Typography.Text>
             </div>
             <Button ghost onClick={()=>auth.signOut()}>Sign out</Button>
         </WrapperStyled>
